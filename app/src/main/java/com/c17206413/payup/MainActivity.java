d6c6216cc4f65831e777d1b40b300ae55e97d451
EoinGohery <37302139+EoinGohery@users.noreply.github.com>
2020-11-14 21:03:44 +0000

    Layout Frame Setup

--- app/src/main/java/com/c17206413/payup/MainActivity.java
+++ app/src/main/java/com/c17206413/payup/MainActivity.java
@@ -9,8 +9,6 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.c17206413.payup.ui.main.SectionsPagerAdapter;
@@ -26,11 +24,12 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.addbutton);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
