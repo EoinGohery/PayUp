c963e0c43d75c4e4a471aa4b9d8ef3a11a4d37c5
EoinGohery <37302139+EoinGohery@users.noreply.github.com>
2021-02-04 19:26:25 +0000

    Stripe onboarding initial commit

--- app/src/main/java/com/c17206413/payup/ui/main/ExpenseActivity.kt
+++ app/src/main/java/com/c17206413/payup/ui/main/ExpenseActivity.kt
@@ -5,10 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.c17206413.payup.R
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.wallet.IsReadyToPayRequest
-import com.google.android.gms.wallet.PaymentsClient
-import com.google.android.gms.wallet.WalletConstants
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.*
 import com.google.firebase.firestore.ktx.firestore
@@ -18,8 +14,6 @@ import com.stripe.android.model.ConfirmPaymentIntentParams
 import com.stripe.android.model.PaymentMethod
 import com.stripe.android.view.BillingAddressFields
 import kotlinx.android.synthetic.main.activity_payment.*
-import org.json.JSONArray
-import org.json.JSONObject
 
 
 class ExpenseActivity : AppCompatActivity() {
@@ -28,10 +22,6 @@ class ExpenseActivity : AppCompatActivity() {
     private lateinit var paymentSession: PaymentSession
     private lateinit var selectedPaymentMethod: PaymentMethod
     private val stripe: Stripe by lazy { Stripe(applicationContext, "pk_test_51HnPJaAXocUznruHqwf1wdNuZeIEEkX9ODwT0yuhtsv9nFPoghcpWbRLDcq3GU0k7g3RlPwCQGhCHVcMPe9nmoqB00JWK66tDF") }
-
-    //private lateinit var paymentsClient: PaymentsClient
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
